1ad72aa91c68cf8355207c3b18095141615fe6fb
hawk86104 <hawk86104@163.com>
2023-12-13 19:30:38 +0800

    优化了 热力图组件

--- src/plugins/digitalCity/common/utils.js
+++ src/plugins/digitalCity/common/utils.js
@@ -1,3 +1,11 @@
+/*
+ * @Description: 
+ * @Version: 1.668
+ * @Autor: 地虎降天龙
+ * @Date: 2023-11-09 09:33:51
+ * @LastEditors: 地虎降天龙
+ * @LastEditTime: 2023-12-13 11:34:54
+ */
 import { BufferAttribute } from 'three'
 
 export const resetUV = (geometry) => {
@@ -17,15 +25,23 @@ export const resetUV = (geometry) => {
 
 export const setGeometryUVForm = (srcGeometry, toGeometry) => {
 	toGeometry.computeBoundingBox()
+	const toroomX = toGeometry.boundingBox.max.x - toGeometry.boundingBox.min.x
+	const toroomY = toGeometry.boundingBox.max.y - toGeometry.boundingBox.min.y
+
 	toGeometry.deleteAttribute('uv')
-	srcGeometry.computeBoundingBox()
+	// srcGeometry.computeBoundingBox()
 	const { max, min } = srcGeometry.boundingBox;
 	const roomX = max.x - min.x
 	const roomY = max.y - min.y
+	// debugger
 	const PuvList = []
 	for (let i = 0; i < toGeometry.attributes.position.count; i++) {
-		PuvList.push((toGeometry.attributes.position.getX(i) - min.x) / roomX)
-		PuvList.push((toGeometry.attributes.position.getY(i) - min.y) / roomY)
+		let tmpU = (toGeometry.attributes.position.getX(i) - min.x) / roomX
+		tmpU = tmpU / (toroomX / roomX)
+		PuvList.push(tmpU)
+		let tmpV = (toGeometry.attributes.position.getY(i) - min.y) / roomY
+		tmpV = tmpV / (toroomY / roomY)
+		PuvList.push(tmpV)
 	}
 	const Puvs = new Float32Array(PuvList)
 	toGeometry.setAttribute('uv', new BufferAttribute(Puvs, 2));
---
+++ src/plugins/digitalCity/components/dataDiv.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="title" v-show="showDiv">dsadsadas</div>
+</template>
+
+<script setup>
+import { ref, watchEffect } from 'vue'
+
+const showDiv = ref(false)
+
+watchEffect(() => {
+	// if (showDiv) {
+	// 	console.log("showDiv:", showDiv)
+	// }
+})
+</script>
+
+<style lang="less" scoped>
+.title {
+	color: white;
+	font-size: 20px;
+	font-weight: bolder;
+	position: absolute;
+	z-index: 99999;
+}
+</style>
