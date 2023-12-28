b7796fbd7570d792298048a042ce45d95c17d412
hawk86104 <hawk86104@163.com>
2023-12-28 15:10:09 +0800

    cityRoad Allright

--- src/plugins/digitalCity/components/roads/roadLight.vue
+++ src/plugins/digitalCity/components/roads/roadLight.vue
@@ -4,23 +4,24 @@
  * @Autor: 地虎降天龙
  * @Date: 2023-10-24 10:36:23
  * @LastEditors: 地虎降天龙
- * @LastEditTime: 2023-12-28 12:01:09
+ * @LastEditTime: 2023-12-28 15:05:39
 -->
 <script setup lang="ts">
 import { ref, watchEffect } from 'vue'
 import { useRenderLoop, useTexture } from '@tresjs/core'
-import { CatmullRomCurve3, Vector3, RepeatWrapping, BackSide, DoubleSide, Color } from 'three'
+import { CatmullRomCurve3, Vector3, RepeatWrapping, BackSide, Color } from 'three'
 import { loadGeojson } from '../../common/utils'
-
 // import transformControlsDebug from '../../components/transformControlsDebug.vue'
 
 const props = withDefaults(
 	defineProps<{
+		geoJson: string
 		color?: string
 		position?: Array<number>
 		radius?: number
 		rotationY?: number
 		scale?: number
+		speed?: number
 	}>(),
 	{
 		color: '#ffff00',
@@ -28,8 +29,12 @@ const props = withDefaults(
 		radius: 2,
 		rotationY: -0.3866683251512052,
 		scale: 1.5083171193254858,
+		speed: 1.0
 	},
 )
+
+const tgRef = ref()
+const tmbmRef = ref()
 
 const { map: pTexture } = await useTexture({
 	map: './plugins/digitalCity/image/line.png'
@@ -38,7 +43,7 @@ pTexture.needsUpdate = true
 pTexture.wrapS = pTexture.wrapT = RepeatWrapping
 pTexture.repeat.set(1, 1)
 
-const linePrimary = await loadGeojson('plugins/digitalCity/geojson/primary.geojson')//secondary
+const linePrimary = await loadGeojson(props.geoJson)
 
 // 一下两个参数 是城市建筑物模型的大小中心点 以及 实际地理位置的偏移和位置， 需要和 建模的人 进行沟通不对，不能直接用
 // 121.511782,31.229849
@@ -56,24 +61,25 @@ for (var i = 0; i < linePrimary.length; i++) {
 }
 
 watchEffect(() => {
-	// if (props.color) {
-	// 	shader.uniforms.uColor.value = new Color(props.color)
-	// }
+	if (props.color) {
+		if (tmbmRef.value) {
+			tmbmRef.value.color = new Color(props.color)
+		}
+	}
 })
 const { onLoop } = useRenderLoop()
 onLoop(({ delta }) => {
-	pTexture.offset.x -= Math.random() / 20
+	pTexture.offset.x -= Math.random() / 20 * props.speed
 })
 
-const tgRef = ref()
 </script>
 
 <template>
 	<!-- <transformControlsDebug :model="tgRef" /> -->
 	<TresGroup ref="tgRef" :position="props.position" :rotation-y="props.rotationY" :scale="props.scale">
-		<TresMesh v-for="(item, index) in curve" :renderOrder="3000">
-			<TresTubeGeometry ref="TresTubeGeometryRef" :args="[item, 64, props.radius, 20/*管道圆润*/, false]" />
-			<TresMeshBasicMaterial :map="pTexture" :side="BackSide" :transparent="true" :color="props.color" />
+		<TresMesh v-for="(item) in curve" :renderOrder="3000">
+			<TresTubeGeometry :args="[item, 64, props.radius, 20/*管道圆润*/, false]" />
+			<TresMeshBasicMaterial ref="tmbmRef" :map="pTexture" :side="BackSide" :transparent="true" :color="props.color" />
 		</TresMesh>
 	</TresGroup>
 </template>
