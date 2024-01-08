a47a8907ce21599f11c6e5ba47b3e8f92ee98f50
hawk86104 <hawk86104@163.com>
2024-01-08 15:56:17 +0800

    增加插件管理的新建插件以及模版文件

--- pluginMaker/index.js
+++ pluginMaker/index.js
@@ -4,7 +4,7 @@
  * @Autor: 地虎降天龙
  * @Date: 2024-01-08 09:01:48
  * @LastEditors: 地虎降天龙
- * @LastEditTime: 2024-01-08 15:03:46
+ * @LastEditTime: 2024-01-08 15:54:14
  */
 const compressing = require('compressing')
 const fs = require('fs')
@@ -29,7 +29,6 @@ function fileExistsWithCaseSync (filepath) {
 	}
 	return fileExistsWithCaseSync(dir)
 }
-
 const packagePlugins = (pluginName) => {
 	if (!pluginName) {
 		console.error('打包插件_:【插件名错误】 ')
@@ -61,7 +60,6 @@ const packagePlugins = (pluginName) => {
 			console.error(err)
 		});
 }
-
 const installPlugins = (pluginName) => {
 	if (!pluginName) {
 		console.error('安装插件_:【插件名错误】 ')
@@ -73,28 +71,76 @@ const installPlugins = (pluginName) => {
 		console.error(`安装插件_:不存在名为:【 ${pluginName} 】的插件包。地址：${packagePath}`)
 		return
 	}
-
 	const pluginPath = './src/plugins/' + pluginName
 	// 检查是否存在此插件目录
 	if (fileExistsWithCaseSync(pluginPath)) {
 		console.error(`安装插件_:已经存在名为:【 ${pluginName} 】的插件。目录地址为：${pluginPath}。 如若需要安装请备份和删除目录`)
 		return
 	}
-
 	// 解压插件 到 目录
 	compressing.zip.uncompress(packagePath, './')
 		.then(() => {
 			console.log(`安装插件_:【 ${pluginName} 】 成功。地址：${pluginPath}。安装文件包请自行删除。`)
 		})
 		.catch(err => {
-			console.error(err);
-		});
-
+			console.error(err)
+		})
 }
+
+const createPlugins = (pluginName) => {
+	if (!pluginName) {
+		console.error('新建插件_:【插件名错误】 ')
+		return
+	}
+	const pluginPath = './src/plugins/' + pluginName
+	// 检查是否存在此插件目录
+	if (fileExistsWithCaseSync(pluginPath)) {
+		console.error(`新建插件_:已经存在名为:【 ${pluginName} 】的插件。目录地址为：${pluginPath}。 请取其他的名字`)
+		return
+	}
+
+	// 创建目录
+	const publicPath = `./public/plugins/${pluginName}/preview`
+	fs.mkdir(publicPath, { recursive: true }, (err) => {
+		if (err) throw err
+		console.log(`新建插件_:静态资源目录已创建:【 ${publicPath} 】`)
+	})
+
+	// 解压对应的模版代码到对应的目录
+	compressing.zip.uncompress('./pluginMaker/template.zip', pluginPath)
+		.then(() => {
+			// 要替换的字符及其替换后的内容
+			const configFile = pluginPath + '/config.js'
+			const replaceCharacters = {
+				'template': pluginName,
+			}
+			// 读取原始文件内容
+			fs.readFile(configFile, 'utf8', (err, data) => {
+				if (err) throw err;
+				// 遍历需要替换的字符列表
+				for (let oldChar in replaceCharacters) {
+					const newChar = replaceCharacters[oldChar]
+					// 在数据中查找并替换指定字符
+					data = data.replace(new RegExp(oldChar, 'g'), newChar)
+				}
+				// 将修改后的内容写入新文件
+				fs.writeFile(configFile, data, 'utf8', (err) => {
+					if (err) throw err;
+					console.log(`新建插件_:【 ${configFile} 】config修改完毕`)
+				})
+			})
+
+			console.log(`新建插件_:【 ${pluginName} 】 成功。地址：${pluginPath}。请自行更改config.js`)
+		})
+		.catch(err => {
+			console.error(err)
+		})
+}
+
 
 let type = args[2]
 if (type === 'create') {
-
+	createPlugins(args[3])
 } else if (type === 'package') {
 	packagePlugins(args[3])
 } else if (type === 'install') {
