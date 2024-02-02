f833aab0ee34528d6c115be63912dabb7b921d17
hawk86104 <hawk86104@163.com>
2024-02-02 16:19:42 +0800

    增加了 来源的说明

--- src/components/forPreview/referenceSource.vue
+++ src/components/forPreview/referenceSource.vue
@@ -2,14 +2,13 @@
  * @Description: 
  * @Version: 1.668
  * @Autor: 地虎降天龙
- * @Date: 2024-01-29 18:57:14
+ * @Date: 2024-01-29 19:30:09
  * @LastEditors: 地虎降天龙
- * @LastEditTime: 2024-01-29 19:26:17
+ * @LastEditTime: 2024-02-02 16:14:46
 -->
-
 <template>
-	<FButton type="info" class="absolute" size="small" style="left: 10px; bottom: 10px;" @click="jump()"
-		v-if="referenceSource">
+	<FButton v-if="referenceSource" type="info" class="absolute" size="small" style="left: 10px; bottom: 10px;"
+		@click="jump">
 		<template #icon>
 			<StarOutlined />
 		</template>参考出处 ：{{ referenceSource.title }}
@@ -20,12 +19,17 @@ import { StarOutlined } from '@fesjs/fes-design/icon'
 import { FButton } from '@fesjs/fes-design'
 import { getOnePluginConfig } from '../../common/utils'
 
-let originalUrl = window.location.href
-let hashPart = originalUrl.split('#')[1] || ''
-let parts = hashPart.split('/')
+const originalUrl = window.location.href
+const hashPart = originalUrl.split('#')[1] || ''
+const parts = hashPart.split('/')
 
-let config = getOnePluginConfig(parts[2], parts[3])
+let config = null
 
+if (parts[2] === "basic") {
+	config = getOnePluginConfig(parts[2], parts[3], parts[4])
+} else {
+	config = getOnePluginConfig(parts[2], parts[3])
+}
 console.log(config)
 const referenceSource = config?.referenceSource
 
