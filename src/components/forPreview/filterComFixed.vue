f57df25ff411cce6e8b9140102f7dd7ecd8ace45
hawk86104 <hawk86104@163.com>
2024-03-25 11:36:16 +0800

    增加 预览页面的 查找 组件的关键字功能

---
+++ src/components/forPreview/filterComFixed.vue
@@ -0,0 +1,63 @@
+<!--
+ * @Description: 
+ * @Version: 1.668
+ * @Autor: 地虎降天龙
+ * @Date: 2023-11-03 16:02:49
+ * @LastEditors: 地虎降天龙
+ * @LastEditTime: 2024-03-25 10:18:06
+-->
+<template>
+	<div class="filterFixed">
+		<FInput v-model="inputValue" class="FInput-input" placeholder="查找的功能，如：城市" clearable @input="textChange">
+			<template #prepend> 检索 </template>
+			<template #suffix>
+				<SearchOutlined />
+			</template>
+		</FInput>
+
+		<a target="_black" href="https://gitee.com/ice-gl/icegl-three-vue-tres">
+			<img src="https://gitee.com/ice-gl/icegl-three-vue-tres/badge/star.svg?theme=dark" alt="gitee-starts">
+		</a>
+		<a target="_black" href="https://space.bilibili.com/410503457" style="top: -1px;">
+			<img alt="bilibili"
+				src="https://img.shields.io/badge/dynamic/json?url=https://api.bilibili.com/x/relation/stat?vmid=410503457&amp;query=data.follower&amp;color=282c34&amp;label=B%E7%AB%99&amp;labelColor=FE7398&amp;logo=data%3Aimage%2Fpng%3Bbase64%2CiVBORw0KGgoAAAANSUhEUgAAAGAAAABgCAYAAADimHc4AAAD7ElEQVR4nO2dW9WrMBCFK6ESkFAJSKiESqgEHCABCZWAhEpAAhL2ecik5dDc%2FpXLBDLfWnlqy0xmJ5BMQnq5CIIgCIIgCIIgCIIgCEIBAHQAemYfrgCunD6wAKAHsEKxALgx+bCQD8%2FS9tmgVqeDr1lLigDgZvDhXso+K9TyTBQRwRJ8AHjntl0Flh5QRAQK%2FmKxPeayWx2OXpBNBKiHvi34b7T2MC4pAvW6twR%2FRwkRKPizBN8CgEcuESj4Lwm+BwBjahEk+H8EwJRKhOaCDzW8e1JLfkUUH1NgmR3XmHffHR1l+72BSs8d7w8U+JDAnZERQMcV+CtUi7dNqFqibB4J7vtrq7xKCuAasbTMXCL4T+5aVk6+2xHUrWdhruAR6HIJcOeu2UHI8zyAe2ytWfEdWz9PVvQ8YAmIQ5dDAB9LFsMVAv8oMO2zAGrC5WNIarRiAuKR9jYEd9pY08aa6uUzIHGRdkgKd8pY0yc1WjEBAqypDYoAG0QAZkQAZkQAZkQAZk4vANQenjsSzS3I%2FwcSbXU5jQBUkRtdf4Rar90v8kSv3+I3ffCCSpk8I%2Fw+lgDkdI%2Fv2rEp2CaiWm1AsDQLlDAD+dlFXLMeAaCSeLZdaSFE5VUQNot38cKuEeBgAsSuG0flVZBmEanbXfNQAsS0fgBYIn2fIu3%2FBBMHEyBmDXlFfA8IzeHb+Ems4WAChKykrVA9ZfsQTL57jXzRg4A5wC%2FA8N4ADiZAZwm2XjW75Qh2KOTfA0p4kygPw28OJcCVgn3nDnYo2EwEYRgGH0qAMyICMCMCMCMCMCMCMCMCMCMCfP3qwHDOQ4AAUekTk8FaBRihJnZdYbvtCGC7LvmkM63GjVDINPFrQgCq5ETXfmMzI90FXzPvfqt7x4rEu%2FZaEcCUxFvgz2zO+BUn6UkoaEEAsptiMSX5e8FoRYCN7cVgb4Vq7U%2FH50Pq4JNP7Qiw8UFnJwcK+tXy+Wj6PLEvPgHSHv5UgwA1IQIwwyFAyLJin9RoxYgAzAQIkPwNmf26busC+OIx5TDqo5nDT+F%2FSS%2F9CYzwb+No49zNy2evkYv0LywGGAXUvp6eSneycqOic0w20k7CNgKE7jJunSGLACTCxF27ylmQc98T5MQUH49swd+I0HPXslLKnT0N+wnkrTKi9JZL%2FL9i1SorMmdeQ4TQQ7OFMxIMzGD45w8nUL1im7efENZLJpgPSw0pfz0cdt4U3230Td%2FTvx2R6d2FrHhEWLkq5PELOMsRPHCPnAZGv1xJteL7jbJiaW3sB2nDvPC%2FosSYvjRQz4cJ6n7KO3rYQL7M+L6nVtfDVRAEQRAEQRAEQRAEIZ5%2FSAXmdfXaoQsAAAAASUVORK5CYII%3D&amp;cacheSeconds=3600">
+		</a>
+	</div>
+</template>
+<script setup lang="ts">
+import { inject } from 'vue'
+import { FInput } from '@fesjs/fes-design'
+import { SearchOutlined } from '@fesjs/fes-design/icon'
+
+const textChange = (e: string) => {
+	// console.log('[input.event] [textChange] e:', e);
+}
+
+const inputValue = inject('filterFixedInputValue')
+
+</script>
+
+<style lang="less" scoped>
+.filterFixed {
+	position: fixed;
+	top: 50px;
+	background-color: #fff;
+	padding: 8px 10px 6px;
+	z-index: 999;
+	box-shadow: -1px 3px 10px rgba(0, 0, 0, 0.2);
+	width: calc(100% - 200px - 34px);
+	display: flex;
+	align-items: flex-end;
+
+	.FInput-input {
+		scale: 0.9;
+		position: relative;
+		width: 20em;
+	}
+
+	a {
+		margin-left: 10px;
+		position: relative;
+	}
+}
+</style>
--- src/plugins/preview.vue
+++ src/plugins/preview.vue
@@ -4,18 +4,19 @@
  * @Autor: 地虎降天龙
  * @Date: 2023-11-18 22:17:49
  * @LastEditors: 地虎降天龙
- * @LastEditTime: 2024-03-12 10:49:58
+ * @LastEditTime: 2024-03-25 11:34:53
 -->
 <template>
     <div class="flex h-full">
         <div class="w-50" style="background-color: #0f1222;">
-            <f-menu mode="vertical" :defaultExpandAll="true" :inverted="true" @select="goto">
+            <f-menu mode="vertical" :defaultExpandAll="true" :expandedKeys="expandedKeys" :inverted="true"
+                @select="goto">
                 <f-sub-menu value="1">
                     <template #icon>
                         <AppstoreOutlined />
                     </template>
                     <template #label>基础功能展示</template>
-                    <template v-for="(bP, pkey) in pluginsConfig">
+                    <template v-for="(bP, pkey) in filteredData">
                         <f-menu-item v-if="pkey === 'basic'" v-for="(onePlugin, okey) in bP.child"
                             :value="onePlugin.name">
                             <template #label>{{ onePlugin.title }}</template>
@@ -27,7 +28,7 @@
                         <PictureOutlined />
                     </template>
                     <template #label>插件中心</template>
-                    <template v-for="(onePlugin, pkey) in  pluginsConfig ">
+                    <template v-for="(onePlugin, pkey) in  filteredData ">
                         <f-menu-item v-if="pkey !== 'basic'" :value="pkey">
                             <template #label>
                                 <EditOutlined style="position: absolute;left: 13px;top: 20px;"
@@ -39,8 +40,9 @@
                 </f-sub-menu>
             </f-menu>
         </div>
-        <div class="flex-1 overflow-scroll" style="height: calc(100vh - 54px);">
-            <template v-for="( onePlugin, pkey ) in  pluginsConfig " :key="pkey">
+        <div class="flex-1 overflow-scroll mt-10 relative right-page-list" style="height: calc(100vh - 42px - 52px);">
+            <filterComFixed />
+            <template v-for="( onePlugin, pkey ) in  filteredData " :key="pkey">
                 <template v-if="pkey === 'basic'">
                     <div style="background-color: #f1f1f2;" v-for="( one, opkey ) in  onePlugin.child " :key="opkey"
                         :ref="el => tabListRef[one.name] = el">
@@ -48,29 +50,31 @@
                     </div>
                 </template>
             </template>
-            <template v-for="( onePlugin, pkey ) in  pluginsConfig " :key="pkey">
+            <template v-for="( onePlugin, pkey ) in  filteredData " :key="pkey">
                 <div style="background-color: #f1f1f2;" v-if="pkey !== 'basic'" :ref="el => tabListRef[pkey] = el">
                     <cardList :onePlugin="onePlugin" />
                 </div>
             </template>
+            <UpCircleOutlined class="toTop" @click="scrollToTop" />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-import { defineRouteMeta } from '@fesjs/fes';
-import { AppstoreOutlined, PictureOutlined, EditOutlined } from '@fesjs/fes-design/icon';
-import { getPluginsConfig } from '../common/utils';
+import { ref, provide, watch } from 'vue'
+import { defineRouteMeta } from '@fesjs/fes'
+import { AppstoreOutlined, PictureOutlined, EditOutlined, UpCircleOutlined } from '@fesjs/fes-design/icon'
+import { getPluginsConfig } from '../common/utils'
 import cardList from '../components/forPreview/cardList.vue'
+import filterComFixed from '../components/forPreview/filterComFixed.vue'
 
 defineRouteMeta({
     name: 'preview',
     title: '开源框架展示',
-});
+})
 
 const tabListRef = ref([])
-let pluginsConfig = getPluginsConfig()
+let pluginsConfig = getPluginsConfig() as any
 const goto = (value: string) => {
     tabListRef.value[value.value]?.scrollIntoView({ behavior: 'smooth', block: 'center', inline: "nearest" })
 }
@@ -88,6 +92,57 @@ const isNew = ((time: string) => {
         }
     }
     return false
+})
+
+const scrollToTop = () => {
+    document.querySelector('.right-page-list')?.scrollTo({ top: 0, behavior: 'smooth' })
+}
+
+const expandedKeys = ref(['1', '2'])
+
+const filterFixedInputValue = ref('')
+provide('filterFixedInputValue', filterFixedInputValue)
+
+function filterObjects(obj: any, searchString: string) {
+    if (!searchString) {
+        return pluginsConfig
+    }
+    const result = {} as any;
+    for (const key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            const item = obj[key];
+            if (typeof item === 'object') {
+                if (key === 'basic') {
+                    const bItem = Object.values(filterObjects(item.child, searchString))
+                    if (bItem.length) {
+                        result[key] = {}
+                        Object.assign(result[key], item, { child: bItem })
+                    }
+                    continue
+                }
+                const hasMatchInTitleOrName = (item.title && item.title.toLocaleLowerCase().includes(searchString)) || (item.name && item.name.toLocaleLowerCase().includes(searchString));
+                if (hasMatchInTitleOrName) {
+                    result[key] = item
+                    continue
+                } else {
+                    const filteredPreview = item.preview.filter((previewItem: any) => {
+                        return (previewItem.name && previewItem.name.toLocaleLowerCase().includes(searchString)) || (previewItem.title && previewItem.title.toLocaleLowerCase().includes(searchString));
+                    });
+                    if (filteredPreview.length > 0) {
+                        result[key] = { ...item, preview: filteredPreview };
+                    }
+                }
+            }
+        }
+    }
+    return result
+}
+let filteredData = ref(pluginsConfig)
+watch(filterFixedInputValue, (newValue: any) => {
+    filteredData.value = filterObjects(pluginsConfig, newValue.toLocaleLowerCase())
+    if (!newValue) {
+        expandedKeys.value = ['1', '2']
+    }
 })
 </script>
 
@@ -114,5 +169,16 @@ const isNew = ((time: string) => {
 
 .fes-menu.is-vertical.is-inverted::-webkit-scrollbar {
     display: none;
+}
+</style>
+<style lang="less" scoped>
+.toTop {
+    position: fixed;
+    right: 20px;
+    bottom: 20px;
+    font-size: 26px;
+    font-weight: bold;
+    color: #0f1222;
+    cursor: pointer;
 }
 </style>
