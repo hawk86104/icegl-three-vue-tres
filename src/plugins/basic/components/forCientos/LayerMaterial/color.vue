6cc930979994e243af9685a580eae2a701ac4a60
hawk86104 <hawk86104@163.com>
2024-04-03 16:57:05 +0800

    在 发布模式下 解决了bug

--- src/plugins/basic/components/forCientos/LayerMaterial/color.vue
+++ src/plugins/basic/components/forCientos/LayerMaterial/color.vue
@@ -4,10 +4,10 @@
  * @Autor: 地虎降天龙
  * @Date: 2024-04-02 18:51:33
  * @LastEditors: 地虎降天龙
- * @LastEditTime: 2024-04-03 11:16:32
+ * @LastEditTime: 2024-04-03 16:45:10
 -->
 <script setup lang="ts">
-import { defineProps, watchEffect, ref } from 'vue'
+import { defineProps, watchEffect, ref, onMounted, onBeforeMount } from 'vue'
 import * as THREE from 'three'
 import { useTresContext } from '@tresjs/core'
 import { ColorProps } from 'lamina/types'
