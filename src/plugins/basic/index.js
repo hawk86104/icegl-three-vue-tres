06f18db1cbcdf4555fb3850ecb13fb8013553964
hawk86104 <hawk86104@163.com>
2024-04-09 11:23:07 +0800

    1、增加git代码更新提示 2、继续优化了下 车子的展示， three 新版的 灯光 光照 是有些区别的

---
+++ src/plugins/basic/index.js
@@ -0,0 +1,5 @@
+
+import Environment from './components/forCientos/useEnvironment/component.vue'
+import Lightformer from './components/forCientos/Lightformer/index.vue'
+
+export { Environment, Lightformer }
--- src/plugins/industry4/pages/showCar.vue
+++ src/plugins/industry4/pages/showCar.vue
@@ -4,21 +4,24 @@
  * @Autor: 地虎降天龙
  * @Date: 2023-11-18 08:51:19
  * @LastEditors: 地虎降天龙
- * @LastEditTime: 2024-04-08 14:17:16
+ * @LastEditTime: 2024-04-09 11:20:50
 -->
 <template>
 	<loading />
 	<TresCanvas v-bind="state" window-size>
-		<TresPerspectiveCamera :position="[5, 1, 5]" :fov="30" :near="1" :far="1000" />
+		<TresPerspectiveCamera :position="[5, 1, 15]" :fov="30" :near="1" :far="1000" />
 		<OrbitControls v-bind="controlsState" />
-		<TresAmbientLight color="#ffffff" intensity="2.5" />
+		<TresAmbientLight :intensity="0.5" />
 		<!-- <TresDirectionalLight color="#ffffff" intensity="5" castShadow v-light-helper :position="[0, 3, 0]" /> -->
-		<TresSpotLight ref="spotLight" :position="[0, 15, 0]" :angle="0.3" :penumbra="1" castShadow :intensity="2" />
+		<!-- <TresSpotLight ref="spotLight" :position="[0, 10, 0]" :angle="Math.PI / 4" :distance="20" v-light-helper
+			:penumbra="0.1" castShadow :intensity="60" :decay="1.5" :focus="0.5" /> -->
+		<TresSpotLight ref="spotLight" :position="[0, 10, 0]" :angle="Math.PI / 4" :distance="20" :penumbra="0.1" castShadow
+			:intensity="60" :decay="1.5" :focus="0.5" />
 		<Suspense>
 			<carModel />
 		</Suspense>
 		<Suspense>
-			<reflectorShaderMesh v-bind="configState" :position="[0, -0.6, 0]" />
+			<reflectorShaderMesh v-bind="configState" :position="[0, -1.17, 0]" />
 		</Suspense>
 		<envLightForCar />
 	</TresCanvas>
@@ -29,7 +32,7 @@ import { reactive, ref, watchEffect } from 'vue'
 import { OrbitControls } from '@tresjs/cientos'
 import * as THREE from 'three'
 
-import reflectorShaderMesh from 'PLS/floor/components/reflectorShaderMesh.vue'
+import { reflectorShaderMesh } from 'PLS/floor'
 import { randomLoading as loading } from 'PLS/UIdemo'
 import envLightForCar from '../components/envLightForCar.vue'
 import carModel from '../components/carModel.vue'
@@ -37,10 +40,10 @@ import carModel from '../components/carModel.vue'
 const spotLight = ref(null as THREE.SpotLight | null)
 watchEffect(() => {
 	if (spotLight.value) {
-		spotLight.value.shadow.mapSize.width = 1024
-		spotLight.value.shadow.mapSize.height = 1024
-		spotLight.value.shadow.camera.near = 1
-		spotLight.value.shadow.camera.far = 100
+		// spotLight.value.shadow.mapSize.width = 1024
+		// spotLight.value.shadow.mapSize.height = 1024
+		// spotLight.value.shadow.camera.near = 1
+		// spotLight.value.shadow.camera.far = 100
 		spotLight.value.shadow.bias = -0.0001
 	}
 })
@@ -53,15 +56,15 @@ const configState = reactive({
 })
 
 const state = reactive({
-	clearColor: '#82839f',
+	clearColor: '#111111',
 	shadows: true,
 	alpha: false,
 	antialias: true,
 	pixelRatio: window.devicePixelRatio,
-	shadowMapType: THREE.BasicShadowMap,
 	outputColorSpace: THREE.SRGBColorSpace,
 	toneMapping: THREE.ACESFilmic,
-	useLegacyLights: true
+	useLegacyLights: true,
+	physicallyCorrectLights: true,
 })
 const controlsState = reactive({
 	autoRotate: true,
