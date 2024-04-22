0c6da9302996a1516f5e373ede2d6646e07a551e
hawk86104 <hawk86104@163.com>
2024-04-22 15:32:49 +0800

    完善了 投射阴影的展示

--- src/plugins/projectionShadow/pages/causticsDemo.vue
+++ src/plugins/projectionShadow/pages/causticsDemo.vue
@@ -1,16 +1,13 @@
 <template>
     <TresCanvas v-bind="state" window-size>
-        <TresPerspectiveCamera :position="[15, 15, 15]" :fov="45" :near="0.1" :far="1000" />
+        <TresPerspectiveCamera :position="[-20, 20, 15]" :fov="45" :near="1" :far="1000" />
         <OrbitControls v-bind="controlsState" />
+        <TresDirectionalLight :position="[10, 2, 4]" :intensity="1" />
+
+        <causticsTorusMesh v-bind="causticsState" />
 
         <Suspense>
-            <whiteFloorMesh v-bind="configFloor" :position="[0, -0.1, 0]" />
-        </Suspense>
-
-        <causticsTorusMesh />
-
-        <Suspense>
-            <skyBox texture="https://opensource-1314935952.cos.ap-nanjing.myqcloud.com/images/skyBox/workshop_blur.jpg" />
+            <groundProjectedEnv :position="[0, -0.1, 0]" />
         </Suspense>
     </TresCanvas>
 </template>
@@ -20,8 +17,9 @@ import { ACESFilmicToneMapping } from 'three'
 import { reactive } from 'vue'
 import { TresCanvas } from '@tresjs/core'
 import { OrbitControls } from '@tresjs/cientos'
-import { skyBoxAmesh as skyBox } from 'PLS/skyBox'
-import { whiteFloorMesh } from 'PLS/floor'
+import { groundProjectedEnv } from 'PLS/skyBox'
+import { Pane } from 'tweakpane'
+
 import causticsTorusMesh from '../components/causticsTorusMesh.vue'
 
 const state = reactive({
@@ -34,10 +32,58 @@ const controlsState = reactive({
     enableDamping: true,
     autoRotate: false,
 })
-const configFloor = reactive({
-    size: [60, 60],
-    color: '#cbcb96',
-    receiveShadow: false,
-    edge: 0.35,
+
+const causticsState = reactive({
+    color: '#ffffff',
+    ior: 1.1,
+    backsideIOR: 1.1,
+    far: 15,
+    worldRadius: 0.3,
+    intensity: 0.05,
+    causticsOnly: false,
+    lightSource: { x: 1, y: 1, z: 1 },
+})
+const paneControl = new Pane({ title: '参数' })
+paneControl.addBinding(causticsState, 'color', {
+    label: '颜色',
+})
+paneControl.addBinding(causticsState, 'ior', {
+    label: '折射系数',
+    min: 0.6,
+    max: 1.3,
+    step: 0.01,
+})
+paneControl.addBinding(causticsState, 'backsideIOR', {
+    label: '折射系数2',
+    min: 0.6,
+    max: 1.3,
+    step: 0.01,
+})
+paneControl.addBinding(causticsState, 'far', {
+    label: '可视距离',
+    min: 0,
+    max: 15,
+    step: 1,
+})
+paneControl.addBinding(causticsState, 'worldRadius', {
+    label: '材质大小',
+    min: 0.001,
+    max: 0.5,
+    step: 0.001,
+})
+paneControl.addBinding(causticsState, 'intensity', {
+    label: '强度',
+    min: 0,
+    max: 1,
+    step: 0.01,
+})
+paneControl.addBinding(causticsState, 'causticsOnly', {
+    label: '只显示投射',
+})
+paneControl.addBinding(causticsState, 'lightSource', {
+    label: '光源位置',
+    x: { min: -1, max: 1 },
+    y: { min: -1, max: 1 },
+    z: { min: -1, max: 1 },
 })
 </script>
--- src/plugins/skyBox/index.js
+++ src/plugins/skyBox/index.js
@@ -4,10 +4,11 @@
  * @Autor: 地虎降天龙
  * @Date: 2024-03-27 22:08:16
  * @LastEditors: 地虎降天龙
- * @LastEditTime: 2024-04-22 10:58:40
+ * @LastEditTime: 2024-04-22 14:45:49
  */
 
 import environmentForLightformers from './components/environmentForLightformers.vue'
 import skyBoxAmesh from './components/skyBoxAmesh.vue'
+import groundProjectedEnv from './components/groundProjectedEnvCom.vue'
 
-export { environmentForLightformers, skyBoxAmesh }
+export { environmentForLightformers, skyBoxAmesh, groundProjectedEnv }
