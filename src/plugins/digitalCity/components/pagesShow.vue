8e619c41c19cbe89f35eba9bf3bc88106ac9cd61
hawk86104 <hawk86104@163.com>
2024-04-22 16:40:22 +0800

    增加了 另外一样式的火

--- src/plugins/digitalCity/components/pagesShow.vue
+++ src/plugins/digitalCity/components/pagesShow.vue
@@ -7,65 +7,64 @@
  * @LastEditTime: 2024-03-22 07:22:18
 -->
 <template>
-	<TresCanvas ref="tcRef" v-bind="state" window-size>
-		<TresPerspectiveCamera ref="perspectiveCameraRef" :position="[600, 750, -1221]" :fov="45" :near="1" :far="100000" />
-		<OrbitControls v-bind="controlsState" />
-		<TresAmbientLight color="#ffffff" />
-		<TresDirectionalLight :position="[100, 100, 0]" :intensity="0.5" color="#ffffff" />
-		<template v-if="props.showBuildings && cityFBX">
-			<buildingsModel :model="cityFBX" />
-			<buildingsLines :builds="cityFBX.city" color="#000" />
-		</template>
+    <TresCanvas ref="tcRef" v-bind="state" window-size>
+        <TresPerspectiveCamera ref="perspectiveCameraRef" :position="[600, 750, -1221]" :fov="45" :near="1" :far="100000" />
+        <OrbitControls v-bind="controlsState" />
+        <TresAmbientLight color="#ffffff" />
+        <TresDirectionalLight :position="[100, 100, 0]" :intensity="0.5" color="#ffffff" />
+        <template v-if="props.showBuildings && cityFBX">
+            <buildingsModel :model="cityFBX" />
+            <buildingsLines :builds="cityFBX.city" color="#000" />
+        </template>
 
-		<slot name="ability"></slot>
-		<TresAxesHelper v-if="props.showAxesHelper" :args="[1000]" :position="[0, 19, 0]" />
-		<TresGridHelper v-if="props.showGridHelper" :args="[6000, 100]" :position="[0, 19, 0]" />
-	</TresCanvas>
+        <slot name="ability"></slot>
+        <TresAxesHelper v-if="props.showAxesHelper" :args="[1000]" :position="[0, 19, 0]" />
+        <TresGridHelper v-if="props.showGridHelper" :args="[6000, 100]" :position="[0, 19, 0]" />
+    </TresCanvas>
 </template>
 
 <script setup lang="ts">
 const props = withDefaults(
-	defineProps<{
-		showBuildings?: boolean
-		autoRotate?: boolean
-		showAxesHelper?: boolean
-		showGridHelper?: boolean
-		disableRender?: boolean
-	}>(),
-	{
-		showBuildings: true,
-		autoRotate: true,
-		showAxesHelper: true,
-		showGridHelper: true,
-		disableRender: false,
-	},
+    defineProps<{
+        showBuildings?: boolean
+        autoRotate?: boolean
+        showAxesHelper?: boolean
+        showGridHelper?: boolean
+        disableRender?: boolean
+    }>(),
+    {
+        showBuildings: true,
+        autoRotate: true,
+        showAxesHelper: true,
+        showGridHelper: true,
+        disableRender: false,
+    },
 )
-import { SRGBColorSpace, BasicShadowMap, NoToneMapping } from 'three';
-import { reactive, ref } from 'vue';
-import { } from '@tresjs/core';
-import { OrbitControls } from '@tresjs/cientos';
+import { SRGBColorSpace, BasicShadowMap, NoToneMapping } from 'three'
+import { reactive, ref } from 'vue'
+import {} from '@tresjs/core'
+import { OrbitControls } from '@tresjs/cientos'
 
-import { loadCityFBX } from '../common/loadCity';
-import buildingsModel from "./buildings/buildingsModelCustomShader.vue";// buildingsModelIncompatible buildingsModelShader buildingsModelCustomShader
-import buildingsLines from "./buildings/buildingsLines.vue";
+import { loadCityFBX } from '../common/loadCity'
+import buildingsModel from './buildings/buildingsModelCustomShader.vue' // buildingsModelIncompatible buildingsModelShader buildingsModelCustomShader
+import buildingsLines from './buildings/buildingsLines.vue'
 
 const state = reactive({
-	clearColor: '#000000',
-	shadows: true,
-	alpha: false,
-	useLegacyLights: true,
-	shadowMapType: BasicShadowMap,
-	outputColorSpace: SRGBColorSpace,
-	toneMapping: NoToneMapping,
-	disableRender: props.disableRender
-});
-const controlsState = reactive({ autoRotate: props.autoRotate, enableDamping: true });
+    clearColor: '#000000',
+    shadows: true,
+    alpha: false,
+    useLegacyLights: true,
+    shadowMapType: BasicShadowMap,
+    outputColorSpace: SRGBColorSpace,
+    toneMapping: NoToneMapping,
+    disableRender: props.disableRender,
+})
+const controlsState = reactive({ autoRotate: props.autoRotate, enableDamping: true })
 
 let cityFBX = null
 if (props.showBuildings) {
-	cityFBX = await loadCityFBX()
+    cityFBX = await loadCityFBX()
 }
 const perspectiveCameraRef = ref()
 const tcRef = ref()
-
 </script>
