2b2b361ed1b55ffe7def1d6c600172220a4e301e
hawk86104 <hawk86104@163.com>
2024-06-03 12:42:42 +0800

    完善描述的说明 goView低代码UI 插件

--- src/plugins/goView/config.js
+++ src/plugins/goView/config.js
@@ -4,12 +4,12 @@
  * @Autor: 地虎降天龙
  * @Date: 2024-05-28 12:11:05
  * @LastEditors: 地虎降天龙
- * @LastEditTime: 2024-05-31 16:17:55
+ * @LastEditTime: 2024-06-03 12:41:53
  */
 export default {
     name: 'goView',
     title: 'goView低代码UI',
-    intro: '使用goView导出的配置，导入到本TvT.js快速完成项目落地。整体文档和视频教程详见：<a style="color: #5384ff;" href="https://gitee.com/dromara/go-view" target="_blank">goView低代码UI结合</a>',
+    intro: '使用goView导出的配置，导入到本TvT.js快速完成项目落地。整体文档和视频教程详见：<a style="color: #5384ff;" href="https://docs.icegl.cn/docs/three-vue-tres/editor/goview.html" target="_blank">goView低代码UI结合</a>',
     version: '0.0.1',
     author: '地虎降天龙',
     website: 'https://gitee.com/hawk86104',
@@ -19,8 +19,11 @@ export default {
     require: [],
     preview: [
         {
-            src: `先使用goView低代码平台<a style="color: #5384ff;" href="https://vue.mtruning.club/" target="_blank">vue.mtruning.club</a>编辑好您的项目，然后点击右下角的导出按钮，获得json配置文件后。
-            后面使用: import { viewChart } from 'PLS/goView' 组件来载入json。快速拉起你的项目。`,
+            src: `
+            1、使用goView低代码平台<a style="color: #5384ff;" href="https://vue.mtruning.club/" target="_blank">vue.mtruning.club</a>编辑好您的项目;</br>
+            2、然后点击右下角的导出按钮，获得json配置文件;</br>
+            3、使用: import { viewChart } from 'PLS/goView' 组件来载入json。快速拉起你的项目。</br>
+            整体文档和视频教程详见：<a style="color: #5384ff;" href="https://docs.icegl.cn/docs/three-vue-tres/editor/usegwjson.html" target="_blank">导入-GoView配置文件</a>`,
             type: 'text',
             name: 'goViewGF',
             url: 'https://vue.mtruning.club',
