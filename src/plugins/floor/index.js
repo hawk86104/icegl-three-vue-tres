778ca1343ea42f2512c06dd8ac1b36da619136dc
hawk86104 <hawk86104@163.com>
2024-06-12 18:30:49 +0800

    no message

--- src/plugins/floor/index.js
+++ src/plugins/floor/index.js
@@ -4,7 +4,7 @@
  * @Autor: 地虎降天龙
  * @Date: 2024-04-03 09:50:51
  * @LastEditors: 地虎降天龙
- * @LastEditTime: 2024-04-25 08:59:27
+ * @LastEditTime: 2024-06-12 18:21:43
  */
 import reflectorDiffuse from './components/reflectorDiffuse.vue'
 import reflectorDUDV from './components/reflectorDUDV.vue'
@@ -12,5 +12,6 @@ import reflectorShaderMesh from './components/reflectorShaderMesh.vue'
 import reflectorMipMap from './components/reflectorMipMap.vue'
 import whiteFloorMesh from './components/whiteFloorMesh.vue'
 import gridPlus from './components/gridPlusCom.vue'
+import videoFloor from './components/videoFloor.vue'
 
-export { reflectorDiffuse, reflectorDUDV, reflectorShaderMesh, reflectorMipMap, whiteFloorMesh, gridPlus }
+export { reflectorDiffuse, reflectorDUDV, reflectorShaderMesh, reflectorMipMap, whiteFloorMesh, gridPlus, videoFloor }
