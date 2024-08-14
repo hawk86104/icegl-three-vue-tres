c8501d50eda8f5b9bc03ac14373cc33e475c4d65
hawk86104 <hawk86104@163.com>
2024-08-14 12:03:50 +0800

    no message

--- src/plugins/operationTool/config.js
+++ src/plugins/operationTool/config.js
@@ -9,8 +9,8 @@ export default {
 	"state": "active",
 	"require": [],
 	"preview": [
-		{ "src": "plugins/operationTool/image/炸开.png", "type": "img", "name": "explode", "title": "炸开与还原" },
-		{ "src": "plugins/operationTool/image/框选.png", "type": "img", "name": "frameSelect", "title": "框选实例" },
+		{ "src": "plugins/operationTool/preview/炸开.png", "type": "img", "name": "explode", "title": "炸开与还原" },
+		{ "src": "plugins/operationTool/preview/框选.png", "type": "img", "name": "frameSelect", "title": "框选实例" },
 		// { "src": "plugins/AMapGIS/preview/cubeMesh.png", "type": "img", "name": "drawArrows", "title": "绘制箭头" },
 	]
 }
