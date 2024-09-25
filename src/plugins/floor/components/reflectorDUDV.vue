ca65b203b196fb08e7f62417e2b37d076ce07b20
hawk86104 <hawk86104@163.com>
2024-09-25 08:34:33 +0800

    优化镜面的一个材质 传参

--- src/plugins/floor/components/reflectorDUDV.vue
+++ src/plugins/floor/components/reflectorDUDV.vue
@@ -4,40 +4,46 @@
  * @Autor: 地虎降天龙
  * @Date: 2023-12-25 11:41:13
  * @LastEditors: 地虎降天龙
- * @LastEditTime: 2024-04-18 11:21:44
+ * @LastEditTime: 2024-09-25 08:33:30
 -->
 <template>
 	<TresGroup :scale="props.scale">
-		<primitive :object="mirror" :position-y="-0.01" />
-		<primitive :object="gridHelp" />
+			<primitive :object="mirror" :position-y="-0.01" />
+			<primitive :object="gridHelp" />
 	</TresGroup>
 </template>
 
 <script lang="ts" setup>
 import * as THREE from 'three'
-import { Mesh, PlaneGeometry, RepeatWrapping, GridHelper } from "three"
+import { Mesh, PlaneGeometry, RepeatWrapping, GridHelper } from 'three'
 import { useTexture } from '@tresjs/core'
 import { Reflector, ReflectorDudvMaterial } from '../lib/alienJS/all.three.js'
 
 import { watchEffect, watch } from 'vue'
-const props = withDefaults(defineProps<{
-	reflectivity?: Number
-	showGridHelper?: boolean
-	scale?: Number
-	ignoreObjects?: THREE.Object3D[]
-	size?: Array<number>
-}>(), {
-	reflectivity: 0.8,
-	scale: 1.0,
-	showGridHelper: true,
-	ignoreObjects: [],
-	size: [10, 10]
-})
+const props = withDefaults(
+	defineProps<{
+			reflectivity?: Number
+			showGridHelper?: boolean
+			scale?: Number
+			ignoreObjects?: THREE.Object3D[]
+			size?: Array<number>
+			mapUrl?: string
+	}>(),
+	{
+			reflectivity: 0.8,
+			scale: 1.0,
+			showGridHelper: true,
+			ignoreObjects: [],
+			size: [10, 10],
+			mapUrl: './plugins/floor/image/waterdudv.jpg',
+	},
+)
+
 const reflector = new Reflector()
 const gridHelp = new GridHelper(props.size[0] - 0.5, props.size[1])
 gridHelp.visible = props.showGridHelper
 
-const { map } = await useTexture({ map: './plugins/floor/image/waterdudv.jpg' })
+const { map } = await useTexture({ map: props.mapUrl })
 map.wrapS = RepeatWrapping
 map.wrapT = RepeatWrapping
 map.repeat.set(6, 3)
@@ -56,38 +62,38 @@ mirror.add(reflector)
 mirror.onBeforeRender = (rendererSelf: any, sceneSelf: any, cameraSelf: any) => {
 	mirror.visible = false
 	props.ignoreObjects.forEach((child: any) => {
-		if (child.isMesh) {
-			child.visible = false
-		}
-		if (child.value && child.value.isMesh) {
-			child.value.visible = false
-		}
+			if (child.isMesh) {
+					child.visible = false
+			}
+			if (child.value && child.value.isMesh) {
+					child.value.visible = false
+			}
 	})
 	reflector.update(rendererSelf, sceneSelf, cameraSelf)
 	props.ignoreObjects.forEach((child: any) => {
-		if (child.isMesh) {
-			child.visible = true
-		}
-		if (child.value && child.value.isMesh) {
-			child.value.visible = true
-		}
+			if (child.isMesh) {
+					child.visible = true
+			}
+			if (child.value && child.value.isMesh) {
+					child.value.visible = true
+			}
 	})
 	mirror.visible = true
 }
 watchEffect(() => {
 	if (props.reflectivity) {
-		material.uniforms.uReflectivity.value = props.reflectivity
+			material.uniforms.uReflectivity.value = props.reflectivity
 	}
 })
 
 watch(
 	() => props.showGridHelper,
 	(newVal) => {
-		gridHelp.visible = newVal
-	}
+			gridHelp.visible = newVal
+	},
 )
 
 defineExpose({
-	reflector
+	reflector,
 })
 </script>
