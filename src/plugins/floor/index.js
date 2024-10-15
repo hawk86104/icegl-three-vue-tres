67468e30a9b1e66f61e45e9b2594a2a936224850
hawk86104 <hawk86104@163.com>
2024-10-15 11:25:09 +0800

    图片地板 暴露

--- src/plugins/floor/index.js
+++ src/plugins/floor/index.js
@@ -4,7 +4,7 @@
  * @Autor: 地虎降天龙
  * @Date: 2024-04-03 09:50:51
  * @LastEditors: 地虎降天龙
- * @LastEditTime: 2024-10-15 11:22:59
+ * @LastEditTime: 2024-10-15 11:24:17
  */
 import reflectorDiffuse from './components/reflectorDiffuse.vue'
 import reflectorDUDV from './components/reflectorDUDV.vue'
@@ -14,5 +14,6 @@ import whiteFloorMesh from './components/whiteFloorMesh.vue'
 import gridPlus from './components/gridPlusCom.vue'
 import videoFloor from './components/videoFloor.vue'
 import digitalGround from './components/digitalGround.vue'
+import imgFloor from './components/imgFloor.vue'
 
-export { reflectorDiffuse, reflectorDUDV, reflectorShaderMesh, reflectorMipMap, whiteFloorMesh, gridPlus, videoFloor, digitalGround }
+export { reflectorDiffuse, reflectorDUDV, reflectorShaderMesh, reflectorMipMap, whiteFloorMesh, gridPlus, videoFloor, digitalGround, imgFloor }
