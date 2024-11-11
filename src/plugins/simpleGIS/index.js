0ef954ad3ac9c71de8dedf58803809fa6f4ed007
hawk86104 <hawk86104@163.com>
2024-11-11 12:08:32 +0800

    no message

--- src/plugins/simpleGIS/index.js
+++ src/plugins/simpleGIS/index.js
@@ -1,4 +1,25 @@
 import tilesBuildings from './components/tilesBuildings.vue'
 import tileMapBuildingsMesh from './components/tileMapBuildingsMesh.vue'
 
-export { tilesBuildings, tileMapBuildingsMesh }
+import mapBoxShow from './components/forThreeTile/mapBoxShow.vue'
+import informationDiv from './components/forThreeTile/informationDiv.vue'
+import threeTileFlyToCom from './components/forThreeTile/flyTo.vue'
+
+import { flyTo, flyToNative, getlinePoints } from './common/utils'
+
+import { showLocation, controlsEvents, getMatrixFromBounds, scaleImg } from './components/forThreeTile/utils.ts'
+
+export {
+    tilesBuildings,
+    tileMapBuildingsMesh,
+    mapBoxShow,
+    informationDiv,
+    threeTileFlyToCom,
+    flyTo,
+    flyToNative,
+    getlinePoints,
+    showLocation,
+    controlsEvents,
+    getMatrixFromBounds,
+    scaleImg,
+}
